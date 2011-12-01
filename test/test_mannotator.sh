07c3d4d4bcfafdbdc7a60ea19062e84fe8baa5b8
Florent Angly <florent.angly@gmail.com>
2011-12-01 05:41:48 +0000

    More tests

--- test/test_mannotator.sh
+++ test/test_mannotator.sh
@@ -12,7 +12,7 @@ echo "Running Mannotator with the nr database..."
 PROTDB=./db/Nr_truncated
 I2A=./db/Nr_mappings_truncated.txt
 OUT=mannotatored_nr.gff
-CMD="mannotator.pl -gffs ${PRODIGAL_GFF} -contigs ${CONTIGS_FASTA} -protdb ${PROTDB} -i2a ${I2A} -seq_embed -threads $THREADS -blastx -out $OUT"
+CMD="mannotator.pl -gffs ${PRODIGAL_GFF} -contigs ${CONTIGS_FASTA} -protdb ${PROTDB} -i2a ${I2A} -seq_embed -threads $THREADS -out $OUT"
 echo $CMD
 eval $CMD
 
@@ -22,6 +22,6 @@ echo "Running Mannotator with the uniref database..."
 PROTDB=./db/Uniref_truncated
 I2A=./db/Uniref_mappings_truncated.txt
 OUT=mannotatored_uniref.gff
-CMD="mannotator.pl -gffs ${PRODIGAL_GFF} -contigs ${CONTIGS_FASTA} -protdb ${PROTDB} -i2a ${I2A} -seq_embed -threads $THREADS -blastx -out $OUT"
+CMD="mannotator.pl -gffs ${PRODIGAL_GFF} -contigs ${CONTIGS_FASTA} -protdb ${PROTDB} -i2a ${I2A} -seq_embed -threads $THREADS -out $OUT"
 echo $CMD
 eval $CMD
