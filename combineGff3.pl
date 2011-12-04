7aa6b05e45a8ccc9057476018c74fa5e3cbda26e
Florent Angly <florent.angly@gmail.com>
2011-12-04 11:12:45 +0000

    Debugging mode for combineGff3.pl

--- combineGff3.pl
+++ combineGff3.pl
@@ -66,6 +66,9 @@ my $options = checkParams();
 my %global_gff_used_list = ();
 my $global_any_inserted = 0;
 
+# turn debugging on (1) or off (0)
+my $debug = 0;
+
 # algorithm based parameters...
 # reject all orfs less than this amount!
 my $global_min_orf_cutoff = 100;
@@ -74,14 +77,14 @@ my $global_min_orf_cutoff = 100;
 my $global_shared_olap_cutoff = 0.1;
 
 # output gff3 file
-my $default_output_file = 'parsed.gff3';
-if(exists $options->{'out'}) { $default_output_file = $options->{'out'}; }
-open my $out_fh, ">", $default_output_file or die "Error: Could not write file $default_output_file\n$!\n";
+my $default_feat_file = 'parsed.gff3';
+if(exists $options->{'out'}) { $default_feat_file = $options->{'out'}; }
+open my $feat_fh, ">", $default_feat_file or die "Error: Could not write file $default_feat_file\n$!\n";
 
 # output todo annotation file
-my $default_ann_file = 'todo.fa';
-if(exists $options->{'ann'}) { $default_ann_file = $options->{'ann'}; }
-open my $ann_fh, ">", $default_ann_file or die "Error: Could not write file $default_ann_file\n$!\n";
+my $default_seq_file = 'todo.fa';
+if(exists $options->{'ann'}) { $default_seq_file = $options->{'ann'}; }
+open my $seq_fh, ">", $default_seq_file or die "Error: Could not write file $default_seq_file\n$!\n";
 
 # first parse the fasta file of contigs to get headers and sequence lengths...
 # really there should only be one guy here...
@@ -119,6 +122,7 @@ foreach my $gff3 (@gff_fns)
         
         # filter out orf if it is too short
         if ($feat->length < $global_min_orf_cutoff) {
+            debugFeature('Not keeping feature '.feat2str($feat), "less than $global_min_orf_cutoff bp") if ($debug);
             next;
         }
 
@@ -136,7 +140,7 @@ foreach my $gff3 (@gff_fns)
 
 
 # print to the output file
-print $out_fh "##gff-version 3\n";
+print $feat_fh "##gff-version 3\n";
 my $gffio = Bio::Tools::GFF->new(-gff_version => 3);
 my $list_handle_ref = \$global_gff_list;
 while(nextInList(\$list_handle_ref) == 1)
@@ -147,24 +151,15 @@ while(nextInList(\$list_handle_ref) == 1)
     my $gff_string = $current_feature->gff_string($gffio);
     my @gff_bits = split /\t/, $gff_string;
 
-    # for rast annotated genes, we just need to get the hypothetical proteins
-    my $seq_entry = ">".$gff_bits[0]."_".$current_node->GO_start."_".$current_node->GO_end."\n".
-                    $global_seq->subseq($current_node->GO_start, $current_node->GO_end)."\n";
-    if($gff_bits[1] eq "FIG")
-    {
-        if($gff_bits[8] =~ /hypothetical/) 
-        { 
-            print $ann_fh $seq_entry;
-        }
+    # Write sequence except if it is a RAST annotated gene
+    unless( ($gff_bits[1] eq 'FIG') && ($gff_bits[8] !~ /hypothetical/) ) {
+        my $seq_entry = '>'.$gff_bits[0].'_'.$current_node->GO_start.'_'.$current_node->GO_end."\n".
+                        $global_seq->subseq($current_node->GO_start, $current_node->GO_end)."\n";
+        print $seq_fh $seq_entry;
     }
-    # for all else, just grab it
-    else
-    {
-        print $ann_fh $seq_entry;
-    }
+    
     # check to see if we'll need to do some annotation afterwards...
-
-    print $out_fh "$gff_string\n";
+    print $feat_fh "$gff_string\n";
 }
 
 
@@ -172,11 +167,36 @@ while(nextInList(\$list_handle_ref) == 1)
 # CUSTOM SUBS
 ######################################################################
 
+sub debugFeature {
+    # display why a feature was kept or not
+    my ($msg, $details) = @_;
+    print "Debug: ".$msg.".\n";
+    print "Reason: $details\n" if defined $details;
+    return 1;
+}
+
+
+sub feat2str {
+    my $feat = shift;
+    return $feat->source_tag." ".$feat->primary_tag." ".$feat->start."-".$feat->end;
+}
+
+
 sub insertFeature {
     #-----
     # insert a feature, provided that it does not displace any existing feature...
-    #
+    # start_ref: the list of features
+    # feat_ref: the feature that we try to put in the list
     my ($start_ref, $feat_ref) = @_;
+
+    ####
+    #use Data::Dumper;
+    #print "FEATURE TO INSERT: ".Dumper($feat_ref);
+    #print "LIST: ".Dumper($start_ref);
+    #print "\n";
+    #die "Temp exit\n";
+    ####
+
     my $feat = ${$feat_ref};
     my $feat_start  = $feat->start;
     my $feat_end    = $feat->end;
@@ -226,6 +246,7 @@ sub insertFeature {
         # there is no need to keep searching the list
         if($current_start > $feat_start)
         {
+            debugFeature("Not keeping feature ".feat2str($$feat_ref), 'no need to keep searching the list') if $debug;
             return $start_ref;
         }
         
@@ -235,6 +256,7 @@ sub insertFeature {
         {
             # the feature to be added is completely contained
             # in an existing feature. no go!
+            debugFeature("Not keeping feature ".feat2str($$feat_ref), 'entirely contained in another feature') if $debug;
             return $start_ref;
         }
 
@@ -286,6 +308,9 @@ sub insertFeature {
         }
         if($should_add == 1)
         {
+            debugFeature("Keeping feature ".feat2str($$feat_ref)) if $debug;
+
+
             my $new_obj = GffObject->new();
             $new_obj->GO_gffRef($feat_ref);
             $new_obj->GO_start($feat_start);
@@ -307,6 +332,7 @@ sub insertInList {
     # Insert the new ref AFTER the given LL ref
     #
     my ($ll_ref, $new_ref) = @_;
+
     my $ll_obj = ${$ll_ref};
     my $new_obj = ${$new_ref};
     
