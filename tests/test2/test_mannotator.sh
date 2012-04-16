d4e3a90cffc0ce9b6b3ffaabde79fa43e9e8ba0d
ctSkennerton <c.skennerton@gmail.com>
2012-04-16 23:14:20 +1000

    updated the tests to work with new API

--- tests/test2/test_mannotator.sh
+++ tests/test2/test_mannotator.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
 
-module load blast/2.2.22
+#module load blast/2.2.22
 
 CONTIGS_FASTA=contigs.fa
 PRODIGAL_GFF=prodigal.gff
 RAST_GFF=rast.gff
 THREADS=1
 
+BINDIR="../../bin/"
+PROG="mannotator"
 
 # Mannotator using nr
 echo "Running Mannotator with the nr database..."
 PROTDB=./db/Nr_truncated
 I2A=./db/Nr_mappings_truncated.txt
 OUT=mannotatored_nr.gff
-CMD="mannotator.pl -gffs ${RAST_GFF},${PRODIGAL_GFF} -contigs ${CONTIGS_FASTA} -protdb ${PROTDB} -i2a ${I2A} -seq_embed -threads $THREADS -out $OUT -keep"
+CMD="${BINDIR}${PROG} -n -gffs ${RAST_GFF},${PRODIGAL_GFF} -contigs ${CONTIGS_FASTA} -protdb ${PROTDB} -i2a ${I2A} --seq-embed -threads $THREADS -out $OUT -keep"
 echo $CMD
 eval $CMD
 echo ""
@@ -24,7 +26,7 @@ echo "Running Mannotator with the uniref database..."
 PROTDB=./db/Uniref_truncated
 I2A=./db/Uniref_mappings_truncated.txt
 OUT=mannotatored_uniref.gff
-CMD="mannotator.pl -gffs ${RAST_GFF},${PRODIGAL_GFF} -contigs ${CONTIGS_FASTA} -protdb ${PROTDB} -i2a ${I2A} -seq_embed -threads $THREADS -out $OUT -keep"
+CMD="${BINDIR}${PROG} -n -gffs ${RAST_GFF},${PRODIGAL_GFF} -contigs ${CONTIGS_FASTA} -protdb ${PROTDB} -i2a ${I2A} --seq-embed -threads $THREADS -out $OUT -keep"
 echo $CMD
 eval $CMD
 echo ""
